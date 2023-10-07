8b6bd0838989df9ad082936b77010be3524a638b
cfernandezvegue <carlos.fernandezvegue@gmail.com>
2023-10-07 23:40:40 +0200

    Installed tailwind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SiderbarMenuComponent } from './components/siderbar-menu/siderbar-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiderbarMenuComponent
   ],
   imports: [
     BrowserModule
