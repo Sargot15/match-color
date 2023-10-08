8a71d022d88ae089f0357297973bf338c3a59d12
cfernandezvegue <carlos.fernandezvegue@gmail.com>
2023-10-08 23:27:01 +0200

    Added sidebar menu

---
+++ src/app/components/siderbar-menu/siderbar-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-siderbar-menu',
+  templateUrl: './siderbar-menu.component.html',
+  styleUrls: ['./siderbar-menu.component.css']
+})
+export class SiderbarMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
